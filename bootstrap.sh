e2427f29136abcfa18ca3a866a949894ecfea636
jovenitto <jovenitto@gmail.com>
2014-04-21 17:00:19 +0100

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+# Install (or update if exists) oh-my-zsh
+if [ ! -d "~/.oh-my-zsh" ]; then
+	cd ~
+	git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+else
+	cd ~/.oh-my-zsh
+	git pull
+	cd ~
+fi
+
+# Install (or update if exists) zsh.syntax-highlighting
+if [ ! -d "~/.oh-my-zshi/custom/plugins/zsh-syntax-highlighting" ]; then
+	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zshi/custom/plugins/zsh-syntax-highlighting
+else
+	cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+	git pull
+	cd ~
+fi
+
