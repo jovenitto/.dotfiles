b4ab821f820ae34415730627ad5cdabd721cde1a
jovenitto <jovenitto@gmail.com>
2014-04-21 17:13:58 +0100

    conditions fot git cloning

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 
 
 # Install (or update if exists) oh-my-zsh
-if [ ! -d "~/.oh-my-zsh" ]; then
+if [ ! -e "~/.oh-my-zsh" ]; then
 	cd ~
 	git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 else
@@ -13,7 +13,7 @@ else
 fi
 
 # Install (or update if exists) zsh.syntax-highlighting
-if [ ! -d "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+if [ ! -e "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
 	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 else
 	cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
