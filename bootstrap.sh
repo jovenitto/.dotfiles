fbe9ac850619c2a8f2af0b297a04b44a0837a1c0
jovenitto <jovenitto@gmail.com>
2014-04-21 17:45:34 +0100

    added comments and echos

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,20 +4,32 @@
 
 # Install (or update if exists) oh-my-zsh
 if [ ! -e ~/.oh-my-zsh ]; then
+	echo "Oh-my-zsh does not exist. Cloning repo..."
 	cd ~
 	git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 else
-	cd ~/.oh-my-zsh
+	echo "Oh-my-zsh exists. Pulling repo..."
+ 	cd ~/.oh-my-zsh
 	git pull
 	cd ~
 fi
 
 # Install (or update if exists) zsh.syntax-highlighting
 if [ ! -e ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+	echo Zsh-syntax-highlighting plugin does not exist. Cloning repo...
 	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 else
+        echo Zsh-syntax-highlighting plugin exists. Pulling repo...
 	cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 	git pull
 	cd ~
+fi
+
+if [ ! -h ~/.zshrc ]; then
+        echo .zshrc is not a link. Deleting and linking to repo...
+	rm -rf ~/.zshrc
+	ln -s ~/.dotfiles/zshrc ~/.zshrc
+else
+        echo .zshrc is a link. Nothing to do.
 fi
 
