aee556a0e7da45ca6ce49f90ec72f4190404c043
jovenitto <jovenitto@gmail.com>
2014-04-22 16:35:37 +0100

    echos

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,11 +11,11 @@ cd ~
 
 # Install (or update if exists) oh-my-zsh
 if [ ! -e ~/.oh-my-zsh ]; then
-	echo \ --\> Cloning oh-my-zsh.
+	echo \ --\> Cloning oh-my-zsh
 	cd ~
 	git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
 else
-	echo \ --\> Pulling oh-my-zsh.
+	echo \ --\> Pulling oh-my-zsh
  	cd ~/.oh-my-zsh
 	git pull &> /dev/null
 	cd ~
@@ -23,10 +23,10 @@ fi
 
 # Install (or update if exists) zsh.syntax-highlighting
 if [ ! -e ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-	echo \ --\> Cloning zsh-syntax-highlighting plugin.
+	echo \ --\> Cloning zsh-syntax-highlighting plugin
 	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &> /dev/null
 else
-    echo \ --\> Pulling zsh-syntax-highlighting plugin.
+    echo \ --\> Pulling zsh-syntax-highlighting plugin
     cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 	git pull &> /dev/null
 	cd ~
@@ -38,7 +38,7 @@ echo Checking simlink files...
 for file in zshrc vimrc
 do
     if [ ! -h ~/.$file ]; then
-        echo \ --\> .$file is not a link. Deleting and linking to repo.
+        echo \ --\> .$file is not a link. Deleting and linking to repo
 	    rm -rf ~/.$file  &> /dev/null
 	    ln -s ~/.dotfiles/$file ~/.$file &> /dev/null
     fi
@@ -47,7 +47,7 @@ done
 echo Checking environment addons...
 
 if [ ! -e ~/.cache/zsh/dirs ]; then
-    echo \ --\> Configuring DirStack.
+    echo \ --\> Configuring DirStack
     cd ~
     mkdir -p ~/.cache/zsh
     touch ~/.cache/zsh/dirs
